274c0a81ea7106b17b7e2493663ec18a75753640
Noah <noahm1216@gmail.com>
2021-12-03 23:05:29 -0800

    Some more notes and basic setup
    - prefabs
    - empty script
    - models of terrain
    - textures of water
    - nothing major

---
+++ BacktraceJam/Assets/Scripts/EcoSystem/Dict_LivingVariables.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// <para>
+/// Dictionary Living Variables is intedned to be the root variables that all living things in the ecosystem can call upon and personalize. It's not unlike a dictionary of Orcs in
+/// a fantasy roleplaying game. Then from that root many variations can be made. Use this to reference and set personal/local variables
+/// </para>
+/// </summary>
+public class Dict_LivingVariables : MonoBehaviour
+{
+    //data here
+
+}//end of dict_livingVariables script
